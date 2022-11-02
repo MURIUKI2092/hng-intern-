13af9370fdcfe50e95ed9e3f835eff7f767db28f
jacksonohallo <jacksonohallo@gmail.com>
2022-11-02 11:30:58 +0300

    add Profile component to handle profile details

---
+++ src/Components/Profile.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {RiArrowRightCircleLine} from "react-icons/ri";
+
+
+function Profile() {
+  return (
+   <>
+        <div className='profile-container'>
+            <div className='profile'> 
+            <img id='profile_img' className='profile-pic' src='images/andrew.jpg' alt='Profile'/>
+            <p id='twitter' className='twitter-name'>Jackson Otieno</p>
+            <p id='slack' className='slack-name'>Jackson</p>
+            </div>
+
+            <div className='react-icon'>
+              <RiArrowRightCircleLine/>
+            </div>
+        </div>
+   </>
+  )
+}
+
+export default Profile;
