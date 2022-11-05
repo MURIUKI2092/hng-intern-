2b18fb40c98608bb44c6cc16bd9ad8131538cd98
James Muriuki <muriukijames33@gmail.com>
2022-11-05 22:16:42 +0300

    refactor: add name to profile page

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -4,8 +4,8 @@ import profilePic from "../images/jackson.png"
 const Profile = () => {
   return (
     <div className="main__content">
-      <img src={profilePic} id="profile__img" className="profile" alt="jackson ohallo" />
-      <h1 id="twitter">ohallo_jackson</h1>
+      <img src={profilePic} id="profile__img" className="profile" alt="jackson otieno" />
+      <h1 id="twitter">Jackson otieno</h1>
       <h1 id="slack" className="hidden">jaseric</h1>
     </div>
   );
