ddc6b0387b2b40cf6239b454ea2c02faeecb9f4b
James Muriuki <muriukijames33@gmail.com>
2022-11-05 20:56:56 +0300

    feat: add footer page

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";import React from "react";
+import ingressive  from "../images/ingressive.jpg";
+
+
+
+const Footer = () => {
+  return (
+    <footer className="App footer__section">
+      <p>HNG Internship 9 Frontend Task</p>
+      <img src={ingressive} alt="ingressive for good" />
+    </footer>
+  );
+};
+
+export default Footer;
