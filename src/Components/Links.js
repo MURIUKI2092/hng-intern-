2d464c13d0059fe7ed4e08c3505b92d7b309d5c2
James Muriuki <muriukijames33@gmail.com>
2022-11-05 21:10:03 +0300

    refactor: add link data and allow mapping

--- src/Components/Links.js
+++ src/Components/Links.js
@@ -1,31 +1,55 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
+const data = [
+  {
+    id: "twitter",
+    link: "https://twitter.com/rioba_riri",
+    title: "@IanRioba",
+  },
+  {
+    id: "btn__zuri",
+    link: "https://training.zuri.team/",
+    title: "Zuri Team",
+  },
+  {
+    id: "books",
+    link: "https://training.zuri.team/",
+    title: "Zuri Team",
+  },
 
+  {
+    id: "book__python",
+    link: "https://books.zuri.team/python-for-beginners?ref_id=IanRioba",
+    title: "Python Books",
+  },
+  {
+    id: "pitch",
+    link: "https://books.zuri.team/",
+    title: "Background  Check for Coders",
+  },
+  {
+    id: "book__design",
+    link: "https://books.zuri.team/design-rules",
+    title: "Design Books",
+  },
+];
 
-function Links() {
+const Links = () => {
   return (
-    <>
-      <div className='links'>
-        <a id='twitter_link' href='https://twitter.com/ohallo_jackson'>Twitter Link</a>
-        <a id='btn_zuri' href='https://training.zuri.team/'>Zuri Team</a>
-        <a title='Find books on design and coding' id='books' href='http://books.zuri.team'>Zuri Books</a>
-        <a title='Buy the best books on python' id='book_python' href='https://books.zuri.team/python-for-beginners?ref_id=pengoJames'>Python Books</a>
-        <a title='Access services for background checks on coders' id='pitch' href='https://background.zuri.team/'>Background Check for Coders</a>
-        <a title='Access design books for free offered by Zuri' id='book_design' href='https://books.zuri.team/design-rules'>Design Books</a>
-      </div>  
-
-      <div className='icons'>
-      <img className='slack-img' src='images/slack.png' alt='Slack Logo'/>
-      <img className='git-hub' src='images/github.png' alt='Github Logo'/>
-      </div>
-
-      <div className='footer'>
-        <p className='intern'>Zuri<span>.</span>Internship</p>
-        <p className='phase'>HNG Internship 9 Frontend Task</p>
-        <img className='ingressive_logo' src='images/ingressive.jpg' alt='Logo'/>
-      </div>
-    </>
-  )
-}
+    <div className="App links">
+      {data.map((e, index) => {
+        return (
+          <a key={index} id={e.id} href={e.link}>
+            {e.title}
+          </a>
+        );
+      })}
+      <Link to="/contact" id="contact">
+        Contact
+      </Link>
+    </div>
+  );
+};
 
 export default Links;
