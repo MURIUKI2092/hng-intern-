541cf0e6a564b4d2491d69675c29eea88ac40253
James Muriuki <muriukijames33@gmail.com>
2022-11-05 21:19:47 +0300

    feat: add home page

---
+++ src/Components/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Footer from "../Footer";
+import Links from "../Links";
+import Profile from "../Profile";
+import Socials from "../Social";
+
+const Home = () => {
+  return (
+    <div className="App col">
+      <Profile />
+      <Links />
+      <Social />
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
